84ca9687003cb875f8c1e93f721b1501dd40885b
Sahil Ghauri <sghauri@albany.edu>
2023-03-17 13:01:26 -0500

    Added mongoDB Connection

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require("express");
-// const fetch = require('node-fetch');
 const axios = require("axios");
+const MongoClient = require("mongodb");
+const { timeStamp, time } = require("console");
+const uri =
+  "mongodb+srv://sghauri:5262@demo-cluster.ljp3wdf.mongodb.net/?retryWrites=true&w=majority";
 
 const hostname = "127.0.0.1";
 const port = 8080;
 
 const server = express();
-// /words?rel_trg=cow
+
 server.get("/words", function (req, res) {
   const word = req.query.rel_syn;
   var user = {
-    agent: req.header('user-agent'), // User Agent we get from headers
-    referrer: req.header('referrer'), //  Likewise for referrer
-    ip: req.header('x-forwarded-for') || req.connection.remoteAddress, // Get IP - allow for proxy
-    screen: { // Get screen info that we passed in url post data
-      width: req.params.height,
-      height: req.params.width
-    }
+    agent: req.header("user-agent"), // User Agent we get from headers
+    referrer: req.header("referrer"), //  Likewise for referrer
+    ip: req.header("x-forwarded-for") || req.connection.remoteAddress, // Get IP - allow for proxy
+    query: word,
+    headersString: JSON.stringify(req.headers),
+    timestamp: Date.now(),
   };
-  // console.log(JSON.stringify(req.headers));
+
+  const client = new MongoClient(uri);
+
+  try {
+    client.connect();
+    client.db("MERN_demo").collection("logs").insertOne(user);
+    console.log("Logged!");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
 
   const API_URL = "https://api.datamuse.com/words?rel_syn=";
   let data = {};
@@ -26,7 +39,7 @@ server.get("/words", function (req, res) {
   axios
     .get(API_URL + word)
     .then(function (response) {
-      console.info("Request received for : "+word+" from : "+user.ip);
+      console.info("Request received for : " + word + " from : " + user.ip);
       data = response.data;
     })
     .catch(function (error) {
